164b5cb4071f0a56e0f048e69ad4db61446be20a
Martin Grimme <martin.grimme@gmail.com>
2021-06-08 17:59:19 +0200

    split up icons into parts instead of one big font

--- icons/webfont.js
+++ icons/webfont.js
@@ -1,16 +1,39 @@
 const webfont = require("webfont");
 const fs = require("fs");
 
-console.log("Creating icon font...");
-webfont.webfont({
-    files: "icons/core/*.svg",
-    fontName: "core",
-    template: "icons/fontmap.template",
-    formats: "woff2"
-})
-.then(result =>
+const items = [
+    { name: "core", target: "lib/style" },
+    { name: "emote", target: "build/icons" },
+    { name: "fs", target: "build/icons" },
+    { name: "map", target: "build/icons" },
+    { name: "text", target: "build/icons" },
+    { name: "ui", target: "build/icons" }
+];
+
+async function makeIconSet(name, target)
 {
+    const result = await webfont.webfont({
+        files: "icons/" + name + "/*.svg",
+        fontName: name,
+        template: "icons/fontmap.template",
+        formats: "woff2"
+    });
     const fontData = Buffer.from(result.woff2, "binary").toString("base64");
-    fs.writeFileSync("lib/style/core-icons.css", result.template.replace("FONT_DATA_GOES_HERE", fontData));
-    console.log("Created font map: core-icons.css");
+    fs.writeFileSync(target + "/" + name + "-icons.css", result.template.replace("FONT_DATA_GOES_HERE", fontData));
+    console.log(" - " + target + "/" + name + "-icons.css");
+}
+
+async function run()
+{
+    for (let i = 0; i < items.length; ++i)
+    {
+        await makeIconSet(items[i].name, items[i].target);
+    }
+}
+
+console.log("Creating icon maps...");
+run()
+.then(() =>
+{
+    console.log("Finished.");
 });

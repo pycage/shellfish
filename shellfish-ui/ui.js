0984a751c3bce5e73b1abb2dc972a9c6b781eef9
Martin Grimme <martin.grimme@gmail.com>
2023-02-18 11:25:26 +0100

    added ListHeader element

--- shellfish-ui/ui.js
+++ shellfish-ui/ui.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
 This file is part of the Shellfish UI toolkit.
-Copyright (c) 2017 - 2022 Martin Grimme <martin.grimme@gmail.com>
+Copyright (c) 2017 - 2023 Martin Grimme <martin.grimme@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
@@ -48,6 +48,7 @@ const mods = [
     "FSView",
     "IndexScroller",
     "Label",
+    "ListHeader",
     "ListViewSelector",
     "Loader",
     "Menu",

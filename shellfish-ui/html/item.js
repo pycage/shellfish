7bcac60bb2de6b8ab7562f3b63098c392190ce18
Martin Grimme <martin.grimme@gmail.com>
2024-01-06 13:30:46 +0100

    fixed rare layout breakage

--- shellfish-ui/html/item.js
+++ shellfish-ui/html/item.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
 This file is part of the Shellfish UI toolkit.
-Copyright (c) 2017 - 2023 Martin Grimme <martin.grimme@gmail.com>
+Copyright (c) 2017 - 2024 Martin Grimme <martin.grimme@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
@@ -1345,7 +1345,8 @@ shRequire(["shellfish/low",
                                    bboxHasChangedWidth ||
                                    bboxHasChangedHeight;
 
-            if (bboxHasChanged && this.ancestorsVisible)
+            //if (bboxHasChanged && this.ancestorsVisible)  // looks like this is too much
+            if (bboxHasChanged)
             {
                 //console.log("BBOX CHANGED " + this.objectType + "@" + this.objectLocation);
                 priv.prevBbox = bbox;
